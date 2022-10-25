a88f3aa1308d0c4d7df5fa4c0a599017ccfaca91
Sinkovskyy <viktorsink202@gmail.com>
2022-10-26 01:20:05 +0300

    Integrate epub reader

---
+++ src/common/books/books.ts
@@ -0,0 +1,5 @@
+export class Books {
+  static readonly MobiDick = 'moby-dick.epub'
+  static readonly MobiDickOnline =
+    'https://www.planetebook.com/free-ebooks/moby-dick.epub'
+}
--- src/screens/Main/Main.tsx
+++ src/screens/Main/Main.tsx
@@ -1,5 +1,34 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {Reader, ReaderProvider} from '@epubjs-react-native/core'
+import {useFileSystem} from '@epubjs-react-native/file-system'
+
+import {SafeAreaView, useWindowDimensions} from 'react-native'
+import {Books} from '@app/common/books'
+
+import {useState} from 'react'
+import RNFS from 'react-native-fs'
 
 export const Main = () => {
-  return <></>
+  const {width, height} = useWindowDimensions()
+
+  const [book, setBook] = useState<string>(Books.MobiDickOnline)
+
+  useEffect(() => {
+    RNFS.readFileAssets(Books.MobiDick, 'base64').then(res => {
+      setBook(res)
+    })
+  }, [])
+
+  return (
+    <SafeAreaView>
+      <ReaderProvider>
+        <Reader
+          src={book}
+          width={width}
+          height={height}
+          fileSystem={useFileSystem}
+        />
+      </ReaderProvider>
+    </SafeAreaView>
+  )
 }
