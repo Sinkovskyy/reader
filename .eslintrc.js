34f221fd551ae53fb15087b329928543f6e62e03
Sinkovskyy <viktorsink202@gmail.com>
2022-10-25 22:45:16 +0300

    Init project

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: ['@react-native-community', 'prettier'],
+  globals: {
+    JSX: true,
+  },
+  ignorePatterns: ['Pods/', 'acceptableUsePolicy.ts'],
+  overrides: [
+    {
+      files: ['src/**/*.tsx', 'src/**/*.ts'],
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'prefer-arrow'],
+  root: true,
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-shadow': 'error',
+    '@typescript-eslint/semi': 'off',
+    'no-shadow': 'off',
+    'no-unexpected-multiline': 'error',
+    'no-var': 'error',
+
+    'prefer-const': 'error',
+    'prettier/prettier': ['error', {endOfLine: 'auto'}],
+    'react-native/no-inline-styles': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    semi: ['error', 'never'],
+  },
+};
